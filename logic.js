32912f08812afde171b8b06e4dfd4333531ce22f
hannahcmtucker <hannahcmtucker@gmail.com>
2017-11-08 14:42:59 +0000

    Add generateAPIcall function

--- logic.js
+++ logic.js
@@ -1,42 +1,48 @@
-function request(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4)
-      if (xhr.status === 200) callback(null, xhr.responseText);
-      else {
-        var errorMessage = xhr.responseText;
-        callback("Error " + url + " " + errorMessage);
-      }
-  }
-};
-xhr.open("GET", url, true);
-xhr.send();
+function request(url, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                cb(null, JSON.parse(xhr.responseText));
+            } else {
+                // if the API returns an error, pass the error into the callback as the first argument
+                var errorMessage = xhr.responseText;
+                cb("Error " + url + " " + errorMessage);
+            }
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
 }
 
-function handleMovieAPI (response) {
-  //copy response
+function generateAPIcall(type, text) {
+    var url = "";
+    if (type === "mov") {
+        url = "https://api.themoviedb.org/3/search/movie?api_key=f76207a8fd7032c7072aa2f3dc514176&query=" + text.split(' ').join('+');
+        request(url, handleMovieAPI);
+    } else if (type === "gif") {
+        url = "http://api.giphy.com/v1/gifs/search?api_key=YK70QDi19ZIBIoIWwHzAlvL9nSV8CXfY&q=" + text;
+        request(url, handleGiphyAPI);
+    }
 
-  //extracts relevant info
+}
 
-  //return info or creates new copy object and populates it
+var handleMovieAPI = function(err, response) {
+    //copy response
+    //extracts relevant info
+    //return info or creates new copy object and populates it
+}
+
+var handleGiphyAPI = function(err, response) {
 
 }
 
 function mapRating() {
-  //input = rating?
-  //output=term
+    //input = rating?
+    //output=term
 }
 
-function createSearchURL() {
-  //input = url, search term
-  //returns url
-}
-
-function handleGiphyAPI() {
-
-}
-
-if (typeof module!--'undefined') {
-  //change below
-  module.exports = test;
+if (typeof module !== 'undefined') {
+    //change below
+    module.exports = test;
 }
