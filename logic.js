46fcd1cca787484a27cf6a6fa92473843f6e42a0
Caroline <carolinelappleby@gmail.com>
2017-11-08 14:08:50 +0000

    did giphy xhr with random

--- logic.js
+++ logic.js
@@ -1,17 +1,44 @@
-function request(url, callback) {
+// function request(url, callback) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function() {
+//     if (xhr.readyState === 4)
+//       if (xhr.status === 200) callback(null, xhr.responseText);
+//       else {
+//         var errorMessage = xhr.responseText;
+//         callback("Error " + url + " " + errorMessage);
+//       }
+//   }
+// };
+// xhr.open("GET", url, true);
+// xhr.send();
+// }
+
+var searchterm = document.getElementById('input');
+
+// function request(url, callback) {
+
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4)
-      if (xhr.status === 200) callback(null, xhr.responseText);
-      else {
-        var errorMessage = xhr.responseText;
-        callback("Error " + url + " " + errorMessage);
-      }
+    if (xhr.readyState === 4 && xhr.status === 200) {
+    var searchResult = document.getElementsByClassName('gif')[0];
+    var newGif = document.createElement('img');
+    var data = JSON.parse(xhr.responseText);
+    console.log(data);
+    var ran = Math.round(Math.random() * 25);
+    newGif.src = data.data[ran].images.original.url;
+    searchResult.appendChild(newGif);
   }
 };
-xhr.open("GET", url, true);
+
+var button = document.getElementById('submit');
+button.addEventListener('click', function() {
+var searchUrl = 'http://api.giphy.com/v1/gifs/search?q=' + input.value.replace(' ', '+') + '&api_key=YK70QDi19ZIBIoIWwHzAlvL9nSV8CXfY';
+
+xhr.open("GET", searchUrl, true);
 xhr.send();
-}
+})
+
+
 
 function handleMovieAPI (response) {
   //copy response
@@ -36,7 +63,7 @@ function handleGiphyAPI() {
 
 }
 
-if (typeof module!--'undefined') {
+if (typeof module!=='undefined') {
   //change below
   module.exports = test;
 }
