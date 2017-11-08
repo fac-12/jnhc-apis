6546426eebabb2f841f3103e3de3b7342f2908f8
Caroline <carolinelappleby@gmail.com>
2017-11-08 16:11:06 +0000

    created handleMoveAPI function

--- logic.js
+++ logic.js
@@ -1,9 +1,9 @@
-function request(url, cb) {
+function request(url, cb, text) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                cb(null, JSON.parse(xhr.responseText));
+                cb(null, JSON.parse(xhr.responseText), text);
             } else {
                 // if the API returns an error, pass the error into the callback as the first argument
                 var errorMessage = xhr.responseText;
@@ -19,23 +19,32 @@ function generateAPIcall(type, text) {
     var url = "";
     if (type === "mov") {
         url = "https://api.themoviedb.org/3/search/movie?api_key=f76207a8fd7032c7072aa2f3dc514176&query=" + text.split(' ').join('+');
-        request(url, handleMovieAPI);
+        request(url, handleMovieAPI, text);
     } else if (type === "gif") {
         url = "http://api.giphy.com/v1/gifs/search?api_key=YK70QDi19ZIBIoIWwHzAlvL9nSV8CXfY&q=" + text;
-        request(url, handleGiphyAPI);
+        request(url, handleGiphyAPI, text);
     }
 
 }
 
-var handleMovieAPI = function(err, response) {
-    //copy response
-    //extracts relevant info
-    //return info or creates new copy object and populates it
+var handleMovieAPI = function(err, response, text) {
+  if (err) {
+    window.alert('Oops! Something went wrong!');
+  } else {
+    var title = response.results[0].title.toLowerCase();
+    if (title === text.toLowerCase()) {
+      var voteAverage = response.results[0].vote_average;
+      mapRating(voteAverage);
+    } else if (result !== text.toLowerCase()) {
+      window.alert('That\'s not a movie! Maybe check your spelling...');
+    }
+  }
+
 }
 
+
 var handleGiphyAPI = function(err, response) {
-
-
+ console.log('working')
 }
 
 // Below function pulls rating from movie API and converts it to a search term for GIPHY
@@ -46,7 +55,7 @@ function mapRating(num){
   generateAPIcall("gif", ratingDesc[ratingRound])
 }
 
-
+generateAPIcall('mov', 'titanic');
 
 if (typeof module !== 'undefined') {
     //change below
