deac6c18315fd20dbd8f128105abbcca691782f1
hannahcmtucker <hannahcmtucker@gmail.com>
2017-11-09 12:32:54 +0000

    Update DOM.js

--- dom.js
+++ dom.js
@@ -1,22 +1,39 @@
-var gifHolder = document.getElementById('gif');
+var results = document.getElementById('results');
 var input = document.getElementById('input');
 var button = document.getElementById('submit');
 
-button.addEventListener('click', function(event){
-  generateAPIcall('mov', input.value);
+button.addEventListener('click', function(event) {
+    generateAPIcall('mov', input.value);
 })
 
-//need to refine replace child node
-function appendGif(url) {
-    var imgElement = document.createElement('img');
-    imgElement.src = url;
-    gifHolder.appendChild(imgElement);
-    if (imgElement.src === null) {
-      imgElement.src.replace(url);
-    }
+function appendData(dataObj) {
+    var container = document.createElement('div');
 
-    //
-    // if (newImg) {
-    //     var oldImg = newImg;
-    //     gifHolder = gifHolder.replaceChild(newImg, oldImg);
+    var title = document.createElement('h2');
+    title.innerText = dataObj.title
+    container.appendChild(title);
+
+
+    var gif = document.createElement('img');
+    gif.src = dataObj.gifURL
+    container.appendChild(gif);
+
+    var poster = document.createElement('img');
+    poster.src = dataObj.posterPath
+    container.appendChild(poster);
+
+    var releaseDate = document.createElement('p');
+    releaseDate.innerText = dataObj.releaseDate
+    container.appendChild(releaseDate);
+
+    var rating = document.createElement('p');
+    rating.innerText = dataObj.voteAverage
+    container.appendChild(rating);
+
+    var synopsis = document.createElement('p');
+    synopsis.innerText = dataObj.synopsis
+    container.appendChild(synopsis);
+
+    results.replaceChild(container, results.firstChild);
+
 }
