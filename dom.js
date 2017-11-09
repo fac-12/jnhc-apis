e22e34655152de91fbbbc6196e3d6d09328c9154
hannahcmtucker <hannahcmtucker@gmail.com>
2017-11-09 12:47:57 +0000

    Accesibility updates

--- dom.js
+++ dom.js
@@ -10,28 +10,34 @@ function appendData(dataObj) {
     var container = document.createElement('div');
 
     var title = document.createElement('h2');
-    title.innerText = dataObj.title
+    title.innerText = dataObj.title;
+    title.setAttribute('tabindex', 0);
     container.appendChild(title);
 
 
     var gif = document.createElement('img');
-    gif.src = dataObj.gifURL
+    gif.src = dataObj.gifURL;
+    gif.alt = "GIF with emotion reflecting whether movie rating is good or bad";
     container.appendChild(gif);
 
     var poster = document.createElement('img');
-    poster.src = dataObj.posterPath
+    poster.src = dataObj.posterPath;
+    poster.alt = "Poster for movie";
     container.appendChild(poster);
 
     var releaseDate = document.createElement('p');
-    releaseDate.innerText = dataObj.releaseDate
+    releaseDate.innerText = dataObj.releaseDate;
+    releaseDate.setAttribute('tabindex', 0);
     container.appendChild(releaseDate);
 
     var rating = document.createElement('p');
-    rating.innerText = dataObj.voteAverage
+    rating.innerText = dataObj.voteAverage;
+    rating.setAttribute('tabindex', 0);
     container.appendChild(rating);
 
     var synopsis = document.createElement('p');
-    synopsis.innerText = dataObj.synopsis
+    synopsis.innerText = dataObj.synopsis;
+    synopsis.setAttribute('tabindex', 0);
     container.appendChild(synopsis);
 
     results.replaceChild(container, results.firstChild);
